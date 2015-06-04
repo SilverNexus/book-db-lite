6ca454f740e1539b5cb4ef94f12b8baf8cd321cc
Daniel Hawkins <a_daniel.hawkins@yahoo.com>
2015-06-04 18:21:07 -0400

    Implemented argument parsing in main perl script.

--- src/bookdblite.pl
+++ src/bookdblite.pl
@@ -6,18 +6,21 @@
 # backing type (hardcover or softcover) and more.
 #
 # Author: Daniel Hawkins
-# Last Modified: 2015-06-03
+# Last Modified: 2015-06-04
 #
 
 use strict;
-use DBD::SQLite3;
+use DBD::SQLite;
 
-my VERSION = "0.0.0-dev";
+my $VERSION = "0.0.0-dev";
 
-my db_loc;
+my $db_loc;
 
 # Parse the arguments passed to the program.
 &parse_args;
+
+# Load the database from the specified location.
+&load_db;
 
 &main_menu;
 
@@ -32,15 +35,48 @@ my db_loc;
 # Handles help and version requests, as well as normal usage.
 #
 sub parse_args{
-    #TODO: Actually parse the args.
-    
+    if ($#ARGV eq -1){
+        print "Not enough arguments supplied to $0.\n";
+        &usage_info;
+    }
+    my $tmp_arg;
+    while ($#ARGV gt -1){
+        $tmp_arg = shift(@ARGV);
+        if ($tmp_arg =~ "-h" || $tmp_arg =~ "--help"){
+            &usage_info;
+        }
+        elsif ($tmp_arg =~ "-v" || $tmp_arg =~ "--version"){
+            &ver_info;
+        }
+        else{
+            # Assume it is the database name.
+            $db_loc = $tmp_arg;
+        }
+    }
 }
 
+#
+# usage_info
+#
+# Prints the help information associated with the program and exits.
+#
 sub usage_info{
-    print "Usage: bookdblite [FLAGS] [path to db]\n";
+    print "Usage: $0 [FLAGS] [path to db]\n";
     print "\n";
     print "Valid flags:\n";
+    print "    -h --help     Prints this usage information and exits.\n";
+    print "    -v --version  Prints version information and exits.\n";
     
+    exit;
+}
+
+#
+# ver_info
+#
+# Prints the version info and exits the program.
+#
+sub ver_info{
+    print "$0 $VERSION\n";
     exit;
 }
 
@@ -54,17 +90,35 @@ sub initialize_db{
     
 }
 
+#
+# load_db
+#
+# Opens the database for read/write, and loads the pcre extension into the db.
+#
+sub load_db{
+    
+}
+
+#
+# main_menu
+#
+# Prints the main menu to the screen and waits for the user to select an option.
+# Directs the user to the correct function given their selection.
+#
 sub main_menu{
-    my selection = 0;
+    my $selection = "0";
     # The main menu will repeat until we exit
-    while (selection != '4'){
+    while ($selection != "4"){
         print "Main Menu\n";
         print " 1. Add a Book\n";
         print " 2. Remove a Book\n";
         print " 3. Perform a Query\n";
-        print " 4. Exit\n"
+        print " 4. Exit\n";
         print "\nEnter your selection: ";
-        read selection;
+        # read selection;
+        
+        # This is temporary so we don't get an infinite loop
+        $selection = "4"
     }
     exit;
 }
