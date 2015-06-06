d1e48782252789ddd77f1250e06caa5518cd82d3
Daniel Hawkins <a_daniel.hawkins@yahoo.com>
2015-06-05 20:36:33 -0400

    Implemented check for sqlite3-pcre in Linux install script.

--- install_linux.sh
+++ install_linux.sh
@@ -2,17 +2,28 @@
 
 #
 # Note: This install script should be run as root to allow for this to
-# be copied to /usr/local/bin
+# be copied to /usr/local/bin.
 #
 # Author: Daniel Hawkins
-# Last Modified: 2015-06-03
+# Last Modified: 2015-06-05
 #
 
-cp src/bookdblite.pl /usr/local/bookdblite.pl
+# We need sqlite3-pcre to work, so check for it.
+# We use find so that if it were to be installed in a nonstandard location,
+# we can still find it. If the name has been changed, well, good luck then.
+find /usr -name "pcre.so" > /usr/local/etc/bookdblite_pcre_loc
 if [ $? != 0 ]; then
-    echo "Failed to copy bookdblite.pl to /usr/local."
+    echo "Failed to find pcre.so and store the result in /usr/local/etc."
+    echo "Make sure sqlite3-pcre is installed and you are running this script as root."
+    exit
+fi
+
+# Copy the perl script
+cp src/bookdblite.pl /usr/local/bin/bookdblite.pl
+if [ $? != 0 ]; then
+    echo "Failed to copy bookdblite.pl to /usr/local/bin."
     echo "Are you running as root?"
     exit
 fi
 # Add a link so we can run with or with file extension
-ln -s /usr/local/bookdblite.pl /usr/local/bookdblite
+ln -s /usr/local/bin/bookdblite.pl /usr/local/bin/bookdblite
