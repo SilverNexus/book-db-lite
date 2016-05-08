a5c165ad1c68b6e6c80e5fccce80b41370eaa1ca
SilverNexus <a_daniel.hawkins@yahoo.com>
2016-05-08 14:19:11 -0400

    Refactor names to be a separate stucture to solve a build error with too many arrays.

--- src/book.h
+++ src/book.h
@@ -2,21 +2,24 @@
 #define BOOK_H
 
 typedef struct {
+    const char * const last;
+    const char * const middle;
+    const char * const first;
+    const char * const suffix;
+} name;
+
+typedef struct {
     const char *title;
     const char *subtitle;
-    // We can have multiple authors, so support that.
-    const char *author_last[];
-    const char *author_middle[];
-    const char *author_first[];
-    const char *author_suffix[];
-    const char *owner_last;
-    const char *owner_middle;
-    const char *owner_first;
-    const char *owner_suffix;
+    name owner;
     int year;
     int quantity;
     const char *ISBN;
     const char *binding_type;
+    // We can have multiple authors, so support that.
+    name *authors;
     // Can also have multiple genres.
     const char *genre[];
 } book;
+
+#endif
